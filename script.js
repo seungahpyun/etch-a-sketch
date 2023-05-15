0bee90cb5cd76d270c67151a48dc53a7246d58db
seungahpyun <seungahpyun@icloud.com>
2023-05-15 14:09:41 +0200

    add backgroudcolor

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let div = document.createElement('div');
-const container = document.getElementById('grid');
+const div = document.createElement('div');
+const grid = document.getElementById('grid');
+const defaultSize = 16;
+let currentSize = 0;
 
-for (let i = 0; i < (16*16); i++) {
-  let div = document.createElement('div');
-  div.classList.add('grid-item');
-  div.id = 'gridItem';
-  div.addEventListener('mouseover', colorChange);
-  container.appendChild(div);
-}
 
 function colorChange(e){
   if (e.type === 'mouseover'){
     e.target.style.backgroundColor = 'black';
   }
+}
+
+function setNewsize(newSize) {
+  currentSize = newSize
+}
+
+
+function setGrid(size){
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  for (let i = 0; i < (size*size); i++) {
+    let div = document.createElement('div');
+    div.classList.add('grid-item');
+    div.addEventListener('mouseover', colorChange);
+    div.addEventListener('mousedown', colorChange);
+    grid.appendChild(div);
+  }
+}
+
+window.onload = () => {
+  setGrid(defaultSize)
 }
