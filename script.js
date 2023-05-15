2c16c79a7f104c3e10d9001bfc9e62e7ceb3d771
seungahpyun <seungahpyun@icloud.com>
2023-05-15 12:14:33 +0200

    adding grid css

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let div = document.createElement('div');
 const container = document.getElementById('grid');
-container.appendChild(div);
-//ADD CLASSNAME AND ID FOR LOOP APPEND 16TIMES
+
+for (let i = 0; i < (16*16); i++) {
+  let div = document.createElement('div');
+  div.classList.add('grid-item');
+  div.id = 'gridItem';
+  container.appendChild(div);
+}
