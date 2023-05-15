8202cba48a3bfe1df1a9cc9fa253d5a18a0b344c
seungahpyun <seungahpyun@icloud.com>
2023-05-15 12:42:13 +0200

    adding mouseover

--- script.js
+++ script.js
@@ -5,5 +5,12 @@ for (let i = 0; i < (16*16); i++) {
   let div = document.createElement('div');
   div.classList.add('grid-item');
   div.id = 'gridItem';
+  div.addEventListener('mouseover', colorChange);
   container.appendChild(div);
+}
+
+function colorChange(e){
+  if (e.type === 'mouseover'){
+    e.target.style.backgroundColor = 'black';
+  }
 }
